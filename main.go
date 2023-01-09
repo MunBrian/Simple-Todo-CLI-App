cf964a5ff543976b3e148023950c8f73e55e39df
Brian Mungai <bryanmungai254@gmail.com>
2023-01-09 22:48:47 +0300

    Added Edit todo functionality

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -51,7 +52,7 @@ func getOption() {
 
 	reader := bufio.NewReader(os.Stdin)
 	//Ask user to enter his/her option
-	fmt.Printf("Enter a an option:\n")
+	fmt.Println("Enter a an option: ")
 	option, _ := reader.ReadString('\n')
 
 	//remove delimiter from string
@@ -61,6 +62,7 @@ func getOption() {
 
 func processOption() {
 	switch userOption {
+	//add task
 	case "1":
 		var task string
 
@@ -86,12 +88,48 @@ func processOption() {
 
 		fmt.Print("Your Task  has successfully been added to list.\n \n")
 
+		//view all todos
 	case "2":
 		//loop through the todos list
 		for _, todo := range todos {
 			//get task
-			fmt.Printf("%v \n\n", todo.task)
+			fmt.Printf("%v: %v \n\n", todo.id, todo.task)
 		}
+
+		//edit task
+	case "3":
+		var taskID string
+		reader := bufio.NewReader(os.Stdin)
+		//Ask user to enter his/her option
+		fmt.Printf("Enter Task ID: \n")
+		data, _ := reader.ReadString('\n')
+
+		taskID = strings.Trim(data, "\n, \r")
+
+		//loop throug each todo
+		for index, todo := range todos {
+			//convert todo.id(int) to string
+			if strconv.Itoa(todo.id) == taskID {
+				fmt.Printf("%v \n", todo.task)
+
+				var newTask string
+				reader := bufio.NewReader(os.Stdin)
+				//Ask user to edit previous task
+				fmt.Printf("Edit task: \n")
+				data, _ := reader.ReadString('\n')
+
+				//remove delimiters
+				newTask = strings.Trim(data, "\n,\r")
+
+				//use index from slice to edit task in struct
+				todos[index].task = newTask
+
+				//display message to user
+				fmt.Println("You successfully updated the task.")
+
+			}
+		}
+
 	default:
 		fmt.Printf("You have choose the wrong option.\n\n")
 	}
